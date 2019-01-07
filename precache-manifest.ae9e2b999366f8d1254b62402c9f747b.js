aa904c19eb2c4abd034edbf8f9d1f3a05f85c199
kabiribraheem <ibraheemkabir9@gmail.com git config --global user.name kaniribraheem git config --global user.email ibraheemkabir9@gmail.com>
2019-01-07 11:14:36 +0100

    Updates

---
+++ precache-manifest.ae9e2b999366f8d1254b62402c9f747b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "d61e1aa246badbf198af",
+    "url": "/react-calculator/static/js/main.d61e1aa2.chunk.js"
+  },
+  {
+    "revision": "217e004b83d12cfd2d72",
+    "url": "/react-calculator/static/js/1.217e004b.chunk.js"
+  },
+  {
+    "revision": "d61e1aa246badbf198af",
+    "url": "/react-calculator/static/css/main.fb3fb930.chunk.css"
+  },
+  {
+    "revision": "094dcea7af4519d7549b05b8e40ca8bc",
+    "url": "/react-calculator/index.html"
+  }
+];
